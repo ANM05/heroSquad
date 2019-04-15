0ea8d3ffab191f3dae2f7d65b74a013b9aadecd9
Antony Nganga <antonynganga05@gmail.com>
2019-04-15 21:56:06 +0300

    Create route and form adding hero to squad

--- src/main/java/heroSquad/App.java
+++ src/main/java/heroSquad/App.java
@@ -72,5 +72,12 @@ public class App {
             model.put("template", "templates/squadDetails.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+        get("squad/:id/heroes/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+            model.put("squads", squad);
+            model.put("template", "templates/squad-hero-form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
