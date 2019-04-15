13b89976823de0b5a944d0c4f267c5d5c113ef89
Antony Nganga <antonynganga05@gmail.com>
2019-04-15 11:58:49 +0300

    Adds tests for squad initialization

--- src/main/java/heroSquad/Squad.java
+++ src/main/java/heroSquad/Squad.java
@@ -1,7 +1,22 @@
 package heroSquad;
 
 public class Squad{
-    public Squad(String squadName, int maxSize, String cause){
+    private String mName;
+    private int mSize;
+    private String mCause;
 
+    public Squad(String squadName, int maxSize, String cause){
+     mName = squadName;
+     mSize = maxSize;
+     mCause = cause;
+    }
+    public String getName(){
+        return mName;
+    }
+    public int getSize(){
+        return mSize;
+    }
+    public String getCause(){
+        return mCause;
     }
 }
--- src/test/java/heroSquad/SquadTest.java
+++ src/test/java/heroSquad/SquadTest.java
@@ -6,8 +6,22 @@ import static org.junit.Assert.*;
 public class SquadTest{
     @Test
     public void Squad_instantiatesCorrectly_true(){
-    Squad testSquad = new Squad("",0,"");
+    Squad testSquad = new Squad("mortal",2,"Justice");
     assertEquals(true, testSquad instanceof Squad);
     }
-
+    @Test
+    public void Squad_squadInstanciatesWithName_mortal(){
+        Squad testSquad = new Squad("mortal", 2,"Justice");
+        assertEquals("mortal", testSquad.getName());
+    }
+    @Test
+    public void Squad_squadInstanciatesWithMaxSize_2(){
+        Squad testSquad = new Squad("mortal",2,"Justice");
+        assertEquals(2, testSquad.getSize());
+    }
+    @Test
+    public void Squad_squadinstantiatesWithCause_justice(){
+        Squad testSquad = new Squad("mortal",2,"Justice");
+        assertEquals("Justice", testSquad.getCause());
+    }
 }
